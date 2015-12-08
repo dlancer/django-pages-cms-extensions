57c972b7f1d41565e6f503bcc06ec372a770473f
dlancer <dmdpost@gmail.com>
2015-12-08 20:56:06 +0500

    version import fixed

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from setuptools import setup, find_packages
-from pages import get_version as get_package_version
+from pagesext import get_version as get_package_version
 
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
