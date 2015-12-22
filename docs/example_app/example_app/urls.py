b0a825a289e17df021f99e8f933b7ea3ae835eb1
dlancer <dmdpost@gmail.com>
2015-12-22 21:01:11 +0500

    requirements updated

--- docs/example_app/example_app/urls.py
+++ docs/example_app/example_app/urls.py
@@ -9,14 +9,14 @@ admin.autodiscover()
 from pages.conf import settings as pages_settings
 from pages.views import PageDetailsView
 
-urlpatterns = patterns('',
-                       url(r'^$', PageDetailsView.as_view(), kwargs={pages_settings.PAGES_PAGE_SLUG_NAME: 'home', }),
-                       url(r'^i18n/', include('django.conf.urls.i18n')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^markitup/', include('markitup.urls')),)
+urlpatterns = ([
+    url(r'^$', PageDetailsView.as_view(), kwargs={pages_settings.PAGES_PAGE_SLUG_NAME: 'home',}),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^markitup/', include('markitup.urls'))
+])
 
-urlpatterns += i18n_patterns('',
-                             url(r'^pages/', include('pages.urls')),)
+urlpatterns += i18n_patterns(url(r'^page/', include('pages.urls', namespace='pages')), )
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
